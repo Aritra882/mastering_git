d72f39c15aab5cc1de445de0fc2799a0530cb370
Aritra <aritrahazra701@gmail.com>
2025-11-22 00:56:52 +0530

    Apply Stash

--- hello.js
+++ hello.js
@@ -1,6 +1,13 @@
 console.log('Hello,Git!');
+<<<<<<< Updated upstream
 
 console.log('Hello,World!');
 
 
 
+=======
+console.log('Hello,Github!');
+// I am doing git and github
+//I am an aspiring developer
+//I am doing B.tech
+>>>>>>> Stashed changes
